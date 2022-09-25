e2fc55fcd3f86bed68b4265b6f2283d996acac23
Matthias Petri <Matthias.Petri@gmail.com>
2022-09-25 16:03:36 -0700

    add max test case

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,8 @@ impl<T: Ord> FromIterator<T> for Rmq {
 
 #[cfg(test)]
 mod tests {
+    use std::cmp::Reverse;
+
     use super::*;
     use proptest::prelude::*;
 
@@ -59,6 +61,40 @@ mod tests {
 
             let min = elems.iter().skip(start).take(len).copied().min().unwrap();
             assert_eq!(elems[min_pos],min);
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn rmq_with_max(
+            elems in prop::collection::vec(any::<u16>(), 1..1000),
+            start in 0usize..1000,
+            len in 1usize..100
+        ) {
+            prop_assume!(start + len < elems.len());
+            let other = elems.clone();
+            let rmq = Rmq::from_iter(other.into_iter().map(Reverse));
+
+            let maxpos = rmq.range_minimum(start..(start+len)).unwrap();
+
+            let max = elems.iter().skip(start).take(len).copied().max().unwrap();
+            assert_eq!(elems[maxpos],max);
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn rmq_works_larger_range(
+            elems in prop::collection::btree_set(any::<u32>(), 1..10000)
+        ) {
+            let elem_vec : Vec<u32> = elems.iter().copied().collect();
+            let other = elems.clone();
+            let rmq = Rmq::from_iter(other);
+
+            let min_pos = rmq.range_minimum(0..elems.len()).unwrap();
+
+            let min = elems.iter().copied().min().unwrap();
+            assert_eq!(elem_vec[min_pos],min);
         }
     }
 }
